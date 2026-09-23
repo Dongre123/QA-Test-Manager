6e3ef6630b8958590c2f313205a849a8a50170a2
Sourabh Dongre <sourabh.dongre@1rivet.com>
2026-09-23 19:52:47 +0530

    Add Playwright login automation

---
+++ tests/login.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+
+test('Valid Login - QA Task Manager', async ({ page }) => {
+
+    await page.goto('http://127.0.0.1:3000');
+
+    await page.fill('#username', 'admin');
+    await page.fill('#password', 'admin123');
+
+    await page.click('button');
+
+    await expect(page.locator('#dashboard-section')).toBeVisible();
+
+});
